2c714fd3231c41b268e6cce757e5f4d86fe60814
KClower <kclower3@gmail.com>
2024-11-21 14:41:23 -0600

    finished day 2 objectives

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,9 +2,16 @@
 
 import sys
 
+
+
 LDI = 0b10000010
 PRN = 0b01000111
 HLT = 0b00000001
+ADD = 0b10100000
+SUB = 0b10100001
+MUL = 0b10100010
+
+
 
 class CPU:
     """Main CPU class."""
@@ -21,31 +28,57 @@ class CPU:
 
         address = 0
 
+        if len(sys.argv) != 2:
+            print("Not Valid")
+            sys.exit(1)
+
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8  (op code 130)
-            0b00000000, #           (reg 0)
-            0b00001000, #           (value 8)
-            0b01000111, # PRN R0    (op code 71)
-            0b00000000, #           (reg 0)
-            0b00000001, # HLT       (value 1)
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8  (op code 130)
+        #     0b00000000, #           (reg 0)
+        #     0b00001000, #           (value 8)
+        #     0b01000111, # PRN R0    (op code 71)
+        #     0b00000000, #           (reg 0)
+        #     0b00000001, # HLT       (value 1)
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
+
+        with open(sys.argv[1]) as f:
+            for line in f:
+                line = line.strip()
+
+                if line == "" or line[0] == "#":
+                    continue
+
+                try:
+                    str_value = line.split("#")[0]
+                    value = int(str_value, 2)
+                except ValueError:
+                    print(f"Invalid Number: {str_value}")
+                    sys.exit(2)
+                
+                self.ram[address] = value
+                address += 1
 
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
-        if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        if op == ADD:
+            self.registers[reg_a] += self.registers[reg_b]
+        elif op == SUB: 
+            self.registers[reg_a] -= self.registers[reg_b]
+        elif op == MUL:
+            self.registers[reg_a] *= self.registers[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
+        
+        
 
     def trace(self):
         """
@@ -66,6 +99,8 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
+
+
 
     def ram_read(self, address):
         return self.ram[address]
@@ -88,13 +123,29 @@ class CPU:
                 register_num = self.ram_read(self.pc + 1)
                 register_value = self.ram_read(self.pc + 2)
                 self.registers[register_num] = register_value
-                self.pc += 3
 
+            elif op == ADD:
+                reg_a = self.ram_read(self.pc + 1)
+                reg_b = self.ram_read(self.pc + 2)
+                self.alu(ADD, reg_a, reg_b)
+
+            elif op == SUB:
+                reg_a = self.ram_read(self.pc + 1)
+                reg_b = self.ram_read(self.pc + 2)
+                self.alu(SUB, reg_a, reg_b)
+
+            elif op == MUL:
+                reg_a = self.ram_read(self.pc + 1)
+                reg_b = self.ram_read(self.pc + 2)
+                self.alu(MUL, reg_a, reg_b)
+                
             elif op == PRN:
                 register_num = self.ram_read(self.pc + 1)
                 print(self.registers[register_num])
-                self.pc += 2
-
+                
             elif op == HLT:
                 sys.exit(1)
+
+            op_len = ((op & 0b11000000) >> 6) +1
+            self.pc += op_len
 
